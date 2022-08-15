bc15baebd85903576e5bbdabfecd0c930e36819d
Aalasa Fidler <aalasafidler@Aalasas-Air.station>
2022-08-15 12:56:17 +0100

    First push with heroku remote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTimeo] = useState(0);
+
+  // setCurrentTime is a 'dispatchSetState' function
+  console.log('Hello?', currentTime, setCurrentTimeo);
+
+  // What is useEffect?
+  /* If you’re familiar with React class lifecycle methods, you can
+  think of useEffect Hook as componentDidMount, componentDidUpdate,
+  and componentWillUnmount combined.
+  By using this Hook, you tell React that your component needs to do
+  something after render. React will remember the function you passed
+  (we’ll refer to it as our “effect”), and call it later after performing
+  the DOM updates.
+  */
+  useEffect(() => {
+    /* We issue a request from the frontend to the backend:
+    Because of the proxy configuration I don't have to use the port
+    5000 URL, I can just use /time, which will make the request
+    go to http://localhost:3000/time */
+    fetch('/api/time').then(res => res.json()).then(data => {
+      document.title = 'It is currently ' + data.time;
+      setCurrentTimeo(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +43,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
